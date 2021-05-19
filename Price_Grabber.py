f6b5c10c341dd150d9a9b69ae03ffeb95b679168
Kourosh Simpkins <k.simpkins0820201@arts.ac.uk>
2021-05-19 15:51:45 +0100

    Trying out XMLHttp
    not sure if this'll work, just trying to figure out how to read data into a p5 file.

--- Price_Grabber.py
+++ Price_Grabber.py
@@ -6,17 +6,17 @@ tickers = []
 
 def get_prices(symbol):
     ticker = yf.Ticker(symbol)
+    delisted = False
 
     try:
         todays_data = ticker.history(period='1d')
         high = str(todays_data['High'][0])
         low = str(todays_data['Low'][0])
         close = str(todays_data['Close'][0])
-
+# who wrote this garbage pile of data tunneling?! Oh I did...
     except IndexError: #If there is an index error then the chances are that the symbol has been delisted for whatever reason. 
-        high = 'n/a'
-        low = 'n/a'
-        close = 'n/a'
+        delisted = True
+        return delisted
     
     except:
         high = 'n/a'
@@ -24,11 +24,13 @@ def get_prices(symbol):
         close = 'n/a'
 
     finally:
+        if delisted:
+            return delisted
         three_vals = {'high':high, 'low':low , 'close':close}
         return three_vals
 
 def store_prices():
-    with open('Stocks_List.txt', mode='r') as f:
+    with open('Stocks_List.txt', mode='r+') as f:
         with open('Stock_Prices.txt', mode='w') as w:
             for ticker in f:
                 temp = {ticker.strip(): get_prices(str(ticker.strip()))}
--- sketch.js
+++ sketch.js
@@ -1,10 +1,33 @@
 function setup() {
   createCanvas(400, 400);
+  readTextFile("file://Stock_Prices.txt");
 }
 
 function draw() {
-  noStroke();
+  strokeWeight(1);
   background(220);
-  fill(600, 609, 594);
-  square(0,0,1);
+  fill(255, 0, 0);
+  stroke(255, 0, 0);
+  point(0,0);
+  point(1,1);
+  stroke(0, 255, 0);
+  point(0,1);
+  point(1,0);
+}
+
+function readTextFile(file) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function() 
+  {
+    if(rawFile.readyState === 4)
+    {
+      if(rawFile.readyState === 200 || rawFile.status == 0)
+      {
+        var allText = rawFile.responseText;
+        alert(allText);
+      }
+    }
+  };
+  rawFile.send(null);
 }
